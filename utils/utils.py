46fb5eb929bd51af1139250212cfb97b090c22f7
jasonlee27 <leej941@gmail.com>
2017-04-12 13:12:55 -0500

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,22 +1,6 @@
-import os
-import csv
-import sys
-import cv2
-import random
-import base64
-import fnmatch
 import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-
-import tensorflow as tf
-from tensorflow.python.platform import gfile
 from keras.utils import np_utils
-from globals import RGB_FEAT_SIZE, AUDIO_FEAT_SIZE, MAX_FRAMES, NUM_CLASSES, \
-    FRM_LVL_FEAT_NAMES, VID_LVL_FEAT_NAMES, GLOBAL_FEAT_NAMES, \
-    VIDEO_TRAIN_DIR, VIDEO_VAL_DIR, VIDEO_TEST_DIR, \
-    FRAME_TRAIN_DIR, FRAME_VAL_DIR, FRAME_TEST_DIR, \
-    EX_DATA_DIR
+
 
 def Dequantize(feat_vector, max_quantized_value=2, min_quantized_value=-2):
     assert max_quantized_value > min_quantized_value
